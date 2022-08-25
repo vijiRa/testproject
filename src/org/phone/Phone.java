6333e0aeb02b77316f796f74f1c45278161bf4b4
vijiRa <vr451996@gmail.com>
2022-08-24 22:07:59 -0700

    new code added

---
+++ src/org/phone/Phone.java
@@ -0,0 +1,23 @@
+package org.phone;
+
+public class Phone {
+	public void phoneInfo() {
+		System.out.println("Phone Info");
+	}
+	public void phoneInfo(String name,int camera) {
+		System.out.println("Phone name "+name);
+		System.out.println("Phone camera "+camera);
+	}
+	public void phoneInfo(int camera, String name) {
+		System.out.println("Phone camera "+camera);
+		System.out.println("Phone name "+name);
+	}
+	public static void main(String[] args) {
+		Phone p = new Phone();
+		p.phoneInfo();
+		p.phoneInfo("Vivo", 48);
+		p.phoneInfo(32, "Samsung");
+	}
+	
+
+}
